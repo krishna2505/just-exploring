3867165074672b7f24d582344002c73e1f153649
krishna2505 <sonikrishna2505@gmail.com>
2026-06-26 11:39:56 +0530

    Changing the console handler level to warning in branch

--- src/myproject/main.py
+++ src/myproject/main.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.WARNING)
 console_handler.setFormatter(formatter)
 
 file_handler = logging.FileHandler('app.log')
